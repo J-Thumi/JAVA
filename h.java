5f5116c9b9270b0ba1609ae9deb0d0aec424be49
Josphat Thumi <145795204+J-Thumi@users.noreply.github.com>
2024-01-27 15:20:40 +0300

    Add files via upload

---
+++ h.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    System.out.println("What is your name");
+    String name=scanner.nextLine();
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
