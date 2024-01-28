7a1321564b5862463e3ed6cd658b71e31d9ff17b
Josphat Thumi <145795204+J-Thumi@users.noreply.github.com>
2024-01-28 19:11:46 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //abstract classes cannot be instantiated ,but they can have a subclass
+        //abstract methods are declared without an implementation
+
+        car car1=new car();
+        car1.go();
+    }
+}
